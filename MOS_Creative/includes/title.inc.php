589854556060394aae50d621be283fdd02aaad37
pioneer903 <pioneer903@gmail.com>
2015-06-22 17:39:28 -0400

    Cleaned out unused files(PSDs, original template files, etc.)
    Signed-off-by: pioneer903 <pioneer903@gmail.com>

--- MOS_Creative/includes/title.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = basename($_SERVER['SCRIPT_NAME'], '.php');
-$title = str_replace('_', ' ', $title);
-if ($title == 'index') {
-  $title = 'Top Marketing Agency | Mobile & Web Design';
-  }
-$title = ucwords($title);
-?>
